95c000c18bbdd7514178825d57d493fddd9d9766
2010 <sirdiegoforlan@gmail.com>
2025-08-08 07:53:59 +0300

    Update and rename 1.-Hello.c to 1.-first_c_Program_and_comments.c

---
+++ 1.-first_c_Program_and_comments.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+
+  // this a comment line 
+
+  /*
+  multiline commaent lines
+  let's start 
+  let's
+  go 
+  to 
+  the
+  rumbleeeeeeeeeee!!!!!!
+  */
+  
+printf("Hello wordl \n");
+  printf("i like pizza");
+  
+  return 0;
+}
