6903aca7aaad3ac4c4896757f2223f1848435186
2010 <sirdiegoforlan@gmail.com>
2025-08-08 13:06:37 +0300

    Create 4.-opertors.c

---
+++ 4.-opertors.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(){
+
+  // aritmethic operators = + - * / % ++ --
+
+  int x = 10;
+  int y = 33;
+  int z = 0;
+
+  //z = x + y;
+  //z = x - y;
+  //z = x / y;
+  //z = x * y;
+
+  //z=z%2; // % is modulus , gives the reminder of the division
+
+ // z++; // it is sam with
+ // z=z+1
+  // this is a incerement
+
+  //z--;  z=z-1;
+
+  //x=+2;
+   
+  
+  
+
+  
+  
+  
+}
