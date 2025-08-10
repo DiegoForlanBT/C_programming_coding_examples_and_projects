bc08f94ac2d94d6b6c2b2d951a55a7082d6641c2
2010 <sirdiegoforlan@gmail.com>
2025-08-10 18:51:42 +0300

    Create project3.-circleAreaAndPerimeterCalculatorAlgo.c

---
+++ project3.-circleAreaAndPerimeterCalculatorAlgo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+
+double area = 0.0 ; 
+double radius = 0.0 ;
+double perimeter = 0.0 ;
+const double pi = 3.14159 ;
+
+ 
+  printf("Enter a radis value : ");
+  scanf("%lf",&radius);
+
+  area = pi * radius * radius ;
+  printf("Area : %lf\n",area);
+
+  perimeter = 2 * pi * radius ;
+  printf("Perimeter : %f\n",perimeter);
+  
+
+return 0;
+  
+}
