268b0dc39965e7ae76e8518557ec517dd1c1598e
2010 <sirdiegoforlan@gmail.com>
2025-08-13 10:41:24 +0300

    Create 15.-while_loops.c

---
+++ 15.-while_loops.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    
+    // while(condition){
+    // some codes }
+    // if , while condition is goes contineu true some codes will be work 
+    
+    /*
+    while(1==1){
+        printf("i am stuck there : ");
+    }
+    */
+    
+    
+    int number = 0 ;
+    
+    while(number<=0){
+        
+        printf("Enter a number greater than 0 : ");
+        scanf("%d",&number);
+        
+    }
+    
+    
+    return 0;
+}
