e4e01aaa0126154c3809e86051162eff7de067d4
2010 <sirdiegoforlan@gmail.com>
2025-08-17 01:22:20 +0300

    Update 20.-2D_arrays.c

--- 20.-2D_arrays.c
+++ 20.-2D_arrays.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ #include <stdio.h>
 
 int main(){
 
@@ -11,8 +11,8 @@ for(int i=0 ; i<2 ; i++){
   }
 }
 
-for(int a=0 ; a<2 ; a++){
-  for(int b=0 ; b<2 ; b++){
+for(int a=0 ; a<2 ; a++){ // rows : satır
+  for(int b=0 ; b<2 ; b++){ // column : sütun
     printf("%d ", dizi[a][b] );
   }
   printf("\n");
