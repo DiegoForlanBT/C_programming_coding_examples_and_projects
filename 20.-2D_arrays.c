c2c9cb436830ea180e31d3530eb6ae0f866c704b
2010 <sirdiegoforlan@gmail.com>
2025-08-16 23:45:44 +0300

    Create 20.-2D_arrays.c

---
+++ 20.-2D_arrays.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+int dizi [2][2] ;
+
+for(int i=0 ; i<2 ; i++){
+  for(int a=0 ; a<2 ;a++){
+    printf("Please enter the coordinated %d and %d array's elemenet : ",i ,a);
+    scanf("%d", &dizi[i][a]);
+  }
+}
+
+for(int a=0 ; a<2 ; a++){
+  for(int b=0 ; b<2 ; b++){
+    printf("%d ", dizi[a][b] );
+  }
+  printf("\n");
+}
+
+return 0 ;
+}
